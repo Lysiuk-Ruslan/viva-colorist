089ad730f7575b429af6db3188d38f30f52ac269
Lisiuk-Ruslan <114481433+Lysiuk-Ruslan@users.noreply.github.com>
2024-07-07 13:49:26 +0300

    Form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ import AboutMe from './AboutMe/AboutMe';
 import Advantages from './Advantages/Advantages';
 import Favor from './Favor/Favor';
 import MyWorks from './MyWorks/MyWorks';
-// import Form from './Form/Form';
+import Form from './Form/Form';
 // import Certificate from './Certificate/Certificate';
 // import Footer from './Footer/Footer';
 
@@ -23,7 +23,7 @@ export const App = () => {
         <Advantages />
         <Favor />
         <MyWorks />
-        {/* <Form /> */}
+        <Form />
         {/* <Certificate />  */}
       </main>
       {/* <Footer /> */}
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,40 +1,53 @@
 import React from 'react';
-/* // import css from './Form.styles.css'; */
+import './Form.styled.scss';
 
 import FormSvg from 'svg/Form.svg';
 
 export default function Form() {
   return (
-    <section>
-      <h2>Залишилися запитання? </h2>
-      <p>Ми з Вами зв’яжемося</p>
-      <p>Залиште Ваші дані </p>
-      <form action="Ім'я">
-        <label htmlFor=""></label>
-        <ul>
-          <li>
-            <input type="Ім'я" name="Ім'я" id="Ім'я" placeholder="Ім'я"></input>
-          </li>
-          <li>
-            <input
-              type="Номер телефону"
-              name="Номер телефону"
-              id="Номер телефону"
-              placeholder="Номер телефону"
-            ></input>
-          </li>
-          <li>
-            <input
-              type="Текст"
-              name="Текст"
-              id="Текст"
-              placeholder="Текст"
-            ></input>
-          </li>
-        </ul>
-        <button>Надіслати</button>
-      </form>
-      <img src={FormSvg} alt="alt-text" width="140" height="142" />
+    <section className="Form">
+      <div className="container">
+        <h2 className="Form-text">Залишилися запитання? </h2>
+        <p className="Form-text-item">Ми з Вами зв’яжемося</p>
+        <p className="Form-list">Залиште Ваші дані </p>
+        <form action="Ім'я">
+          <label htmlFor=""></label>
+          <ul className="Form-subject">
+            <li className="Form-subject-slogan">
+              <input
+                type="Ім'я"
+                name="Ім'я"
+                id="Ім'я"
+                placeholder="Ім'я"
+              ></input>
+            </li>
+            <li className="Form-subject-slogan">
+              <input
+                type="Номер телефону"
+                name="Номер телефону"
+                id="Номер телефону"
+                placeholder="Номер телефону"
+              ></input>
+            </li>
+            <li className="Form-subject-text">
+              <input
+                type="Текст"
+                name="Текст"
+                id="Текст"
+                placeholder="Текст"
+              ></input>
+            </li>
+          </ul>
+          <button className="Form-btn">Надіслати</button>
+        </form>
+        <img
+          className="Form-icon"
+          src={FormSvg}
+          alt="alt-text"
+          width="140"
+          height="142"
+        />
+      </div>
     </section>
   );
 }
