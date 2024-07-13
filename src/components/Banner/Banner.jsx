48dba043302b37f61b378909c5153dbf28442fee
Lisiuk-Ruslan <114481433+Lysiuk-Ruslan@users.noreply.github.com>
2024-07-13 19:11:27 +0300

    Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,11 +4,17 @@ import './Banner.styled.scss';
 export default function Banner() {
   return (
     <section className="Banner">
-      <div className="container ">
-        <p className="BannerSubject">Студія краси</p>
-        <h1 className="BannerSubtitle">“V’IVA colorist”</h1>
-        <p className="BannerText">Знаємо, як підкреслити Вашу природню красу</p>
-        <button className="BannerButton">Хочу записатися</button>
+      <div className="container BannerGrup ">
+        <div className="qwerty">
+          <p className="BannerSubject">Студія краси</p>
+          <h1 className="BannerSubtitle">“V’IVA colorist”</h1>
+          <p className="BannerText">Знаю, як підкреслити Вашу природню красу</p>
+          <button className="BannerButton">Хочу записатися</button>
+        </div>
+        <div className="BannerImage">
+          <div className="BannerFoto"></div>
+        </div>
+        <div className="qwe"></div>
       </div>
     </section>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,127 @@
 import React from 'react';
-// import css from './Header.styles.css';
 
-import vectorLogo from 'svg/Vectorlogo.svg';
+import './Header.styled.scss';
+import './Mobile.js';
+
+// import vectorLogo from 'svg/Logo.svg';
+
+// export default function Header() {
+//   return (
+//     <header>
+//       <nav>
+//         <a href="/">
+//           <img src={vectorLogo} alt="alt-text" width="300" height="45" />
+//         </a>
+//         <ul>
+//           <li>
+//             <a href="/">Про мене</a>
+//           </li>
+//           <li>
+//             <a href="/">Галерея</a>
+//           </li>
+//           <li>
+//             <a href="/">Послуги</a>
+//           </li>
+//           <li>
+//             <a href="/">Контакти</a>
+//           </li>
+//         </ul>
+//         <a href="tel:+380973901222">+380973901222</a>
+//       </nav>
+//     </header>
+//   );
+// }
+
+// -----------------------------------------------------------------------
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <a href="/">
-          <span>V’IVA</span>
-          <img src={vectorLogo} alt="alt-text" width="50" height="60" />
-          <span>colorist</span>
-        </a>
-        <ul>
+    <header className="page-header">
+      <div className="container">
+        <button
+          className="menu-toggle js-open-menu"
+          aria-expanded="false"
+          aria-controls="mobile-menu"
+        >
+          <svg
+            width="24"
+            height="24"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+
+        <ul className="menu">
           <li>
-            <a href="/">Про мене</a>
+            <a href="" className="link">
+              About
+            </a>
           </li>
           <li>
-            <a href="/">Галерея</a>
+            <a href="" className="link">
+              Products
+            </a>
           </li>
           <li>
-            <a href="/">Послуги</a>
+            <a href="" className="link">
+              Contacts
+            </a>
           </li>
           <li>
-            <a href="/">Контакти</a>
+            <a href="" className="link">
+              Career
+            </a>
           </li>
         </ul>
-        <a href="tel:+380973901222">+380973901222</a>
-      </nav>
+      </div>
+
+      <div className="menu-container js-menu-container" id="mobile-menu">
+        <button className="menu-toggle js-close-menu">
+          <svg
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+
+        <ul className="mobile-menu">
+          <li>
+            <a href="" className="link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="" className="link">
+              Products
+            </a>
+          </li>
+          <li>
+            <a href="" className="link">
+              Contacts
+            </a>
+          </li>
+          <li>
+            <a href="" className="link">
+              Career
+            </a>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
